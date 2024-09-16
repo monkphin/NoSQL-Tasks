3b131e4ba375531dfa90842020ac7d671ee2d9d9
Darren Burrows <darren.burrows@gmail.com>
2024-09-16 16:49:19 +0000

    add task edit and done buttons

--- app.py
+++ app.py
@@ -116,6 +116,24 @@ def add_task():
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
+@app.route("/edit_task", methods=["GET", "POST"])
+def edit_task():
+    if request.method == "POST":
+        task = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update_one(task)
+        flash("Task successfully updated!")
+        return redirect(url_for("get_tasks"))
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return redirect(url_for("get_tasks"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
